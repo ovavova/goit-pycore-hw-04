b29c13dcedc2a8585d5bca5e37106db9b9180b9b
Ovavova <vyereschenko@gmail.com>
2025-10-19 13:28:41 +0300

    exercise 2 done

--- 02_get_cats_info.py
+++ 02_get_cats_info.py
@@ -1,5 +1,27 @@
 
-input_path = "cats.txt"
+input_path = "catds.txt"
 
-def get_cats_info(pathj: str) -> list:
-    print()
+def get_cats_info(path: str) -> list:     # Taking path as string  
+    cat_lst = []                          # Creating empti list to store cats dicts
+    cat_dict= {}                          # Empty dictionaory to store each cat info
+    
+    try:
+        with open(path, "r", encoding='utf-8') as f:
+            while True:
+                line = f.readline()       # Read a line from file
+                if not line:
+                    break
+                cat_info = line.split(sep=',')          # Getting list of strings from file about one cat - one string
+                cat_dict = {"id": cat_info[0],          # Adding values from string to dictionary and converting age to int) 
+                        "name": cat_info[1],
+                        "age": int(cat_info[2].strip())}
+                cat_lst.append(cat_dict)                # adding cats dictionary to list of cats dictionaries
+    except FileNotFoundError:                           # Catching wrong file path input error
+        print("File not found! Please provide  valid file path")
+    return cat_lst
+            
+
+print(get_cats_info(input_path))
+
+    
+    
